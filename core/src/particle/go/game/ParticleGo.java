9f282a281a4367639e852b0bd45afca2e1300555
TheHomster2 <anthonyhom2@hotmail.com>
2018-06-16 17:04:50 -0400

    add magnets

--- core/src/particle/go/game/ParticleGo.java
+++ core/src/particle/go/game/ParticleGo.java
@@ -1,20 +1,24 @@
 package particle.go.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+import particle.go.game.model.Grid;
 import particle.go.game.model.Magnet;
+import particle.go.game.model.Player;
 
 public class ParticleGo extends ApplicationAdapter{
 	//private SpriteBatch batch;
 	//private Texture img;
 	private ShapeRenderer mRenderer;
 	private OrthographicCamera mCamera;
+	private Grid grid;
+	private Player[] players;
+	private int p_turn = 0;
 
 	@Override
 	public void create () {
@@ -22,14 +26,26 @@ public class ParticleGo extends ApplicationAdapter{
 		//img = new Texture("badlogic.jpg");
 		mRenderer = new ShapeRenderer();
 		mCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		grid = new Grid(200, 200, 100, 100);
+		players = new Player[2];
+		for (int i = 0; i < 2; i++)
+			players[i] = new Player();
 
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean touchDown (int x, int y, int pointer, int button) {
-                // your touch down code here
+            	if (players[p_turn].turn(grid, x, y))
+					switch_turn();
                 return true; // return true to indicate the event was handled
             }
         });
+	}
+
+	private void switch_turn() {
+		if (p_turn == 1)
+			p_turn = 0;
+		else
+			p_turn = 1;
 	}
 
 	@Override
