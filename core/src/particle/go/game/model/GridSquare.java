ab40a1eebd7c2219f6185880331a8ea9e8bee15f
Stanley Lin <slin55055@gmail.com>
2018-06-16 22:29:38 -0400

    ?

---
+++ core/src/particle/go/game/model/GridSquare.java
@@ -0,0 +1,27 @@
+package particle.go.game.model;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+
+public class GridSquare {
+    private int indexX;
+    private int indexY;
+    private Rectangle mRectangle;
+
+    public GridSquare(Grid grid, int x, int y) {
+        mRectangle = new Rectangle();
+        mRectangle.x = x;
+        mRectangle.y = y;
+        mRectangle.width = grid.getBoxWidth();
+        mRectangle.height = grid.getBoxHeight();
+    }
+
+    public void drawGamePiece(int gridX, int gridY, float width, float height,
+                              ShapeRenderer renderer, GamePiece piece) {
+        piece.drawInBox(gridX, gridY, width, height, renderer);
+    }
+
+    public boolean contains(int screenX, int screenY) {
+        return mRectangle.contains(screenX, screenY);
+    }
+}
