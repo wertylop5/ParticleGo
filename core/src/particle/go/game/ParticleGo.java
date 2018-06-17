73c35287bf3b871d8babdcd6c3fdea1b02a4ef56
Stanley Lin <slin55055@gmail.com>
2018-06-16 21:12:08 -0400

    zoom kinda works

--- core/src/particle/go/game/ParticleGo.java
+++ core/src/particle/go/game/ParticleGo.java
@@ -3,9 +3,12 @@ package particle.go.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
 import particle.go.game.model.Grid;
 import particle.go.game.model.Magnet;
@@ -31,14 +34,69 @@ public class ParticleGo extends ApplicationAdapter{
 		for (int i = 0; i < 2; i++)
 			players[i] = new Player();
 
-        Gdx.input.setInputProcessor(new InputAdapter() {
+		InputMultiplexer multiInput = new InputMultiplexer() {
+			@Override
+			public boolean touchDown (int x, int y, int pointer, int button) {
+				System.out.println("touch down");
+				if (players[p_turn].turn(grid, x, y))
+					switch_turn();
+				return true; // return true to indicate the event was handled
+			}
+		};
+		multiInput.addProcessor(new GestureDetector(new GestureDetector.GestureAdapter(){
+			@Override
+			public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2,
+								 Vector2 pointer1, Vector2 pointer2) {
+				float dX = pointer1.x - initialPointer1.x +
+						pointer2.x - initialPointer2.x;
+				float dY = pointer1.y - pointer1.y +
+						pointer2.y - initialPointer2.y;
+				float mag = (float)Math.sqrt(dX*dX + dY*dY);
+
+				System.out.println("Zoom: " + mCamera.zoom);
+				mCamera.zoom *= mag;
+				System.out.println("Zoom after: " + mCamera.zoom);
+				mCamera.update();
+				return true;
+			}
+		}));
+        /*Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean touchDown (int x, int y, int pointer, int button) {
             	if (players[p_turn].turn(grid, x, y))
 					switch_turn();
                 return true; // return true to indicate the event was handled
             }
-        });
+        });*/
+		Gdx.input.setInputProcessor(new GestureDetector(new GestureDetector.GestureAdapter() {
+			@Override
+			public boolean zoom(float initialDistance, float distance) {
+				float change = -(distance - initialDistance);
+				/*if (change < 0) {
+					change = -(1/change);
+				}*/
+				float percent = change / 300f;//max
+				System.out.println("change: " + change);
+				//System.out.println("change adjusted: " + (float)(10 - (10 - .05)*percent));
+				System.out.println("change adjusted: " + change/3000);
+
+				System.out.println("Zoom: " + mCamera.zoom);
+				mCamera.zoom += change/3000;
+				//mCamera.zoom = (float)(10 - (10 - .05)*percent);
+				System.out.println("zoom before clamp: " + mCamera.zoom);
+				mCamera.zoom = clamp(.5f, 10f, mCamera.zoom);
+				System.out.println("Zoom after: " + mCamera.zoom);
+				mCamera.update();
+				return true;
+			}
+		}));
+		//Gdx.input.setInputProcessor(multiInput);
+	}
+
+	private float clamp(float min, float max, float val) {
+		val = val < min ? min : val;
+		val = val > max ? max : val;
+		return val;
 	}
 
 	private void switch_turn() {
@@ -50,7 +108,7 @@ public class ParticleGo extends ApplicationAdapter{
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 //		batch.begin();
 //		batch.draw(img, 0, 0);
